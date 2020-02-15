87c0c048d113965f928ce01c57ea381d892fee49
wj <wj@wj.com>
2020-02-15 20:37:07 +0800

    modify

--- hellogit/src/com/hellogit/test/HelloGit.java
+++ hellogit/src/com/hellogit/test/HelloGit.java
@@ -5,4 +5,8 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("hello git");
     }
+
+    public void modifyForGit() {
+        System.out.println("modify for commit");
+    }
 }
