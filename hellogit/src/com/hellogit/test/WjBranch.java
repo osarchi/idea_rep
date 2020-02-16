f47cf48ebc297a7bb7cb4332e4c3ddc3f9fd7fc3
wj <wj@wj.com>
2020-02-16 13:41:37 +0800

    add method

--- hellogit/src/com/hellogit/test/WjBranch.java
+++ hellogit/src/com/hellogit/test/WjBranch.java
@@ -4,4 +4,7 @@ public class WjBranch {
     public static void main(String[] args) {
         System.out.println("wj branch class");
     }
+    public void addMethod() {
+        System.out.println("add method");
+    }
 }
